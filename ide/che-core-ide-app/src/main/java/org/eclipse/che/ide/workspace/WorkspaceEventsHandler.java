698da1129c780508f717c6bc43ab42af9e182047
Vitaliy Guliy <vguliy@codenvy.com>
2016-10-26 17:10:19 +0300

    CODENVY-932 Stopped IDE should provide clear state and options to the user

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/workspace/WorkspaceEventsHandler.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/workspace/WorkspaceEventsHandler.java
@@ -117,6 +117,8 @@ public class WorkspaceEventsHandler {
     @VisibleForTesting
     WsAgentOutputSubscriptionHandler     wsAgentLogSubscriptionHandler;
 
+    private boolean workspaceEventsHandled;
+
     @Inject
     WorkspaceEventsHandler(final EventBus eventBus,
                             final CoreLocalizationConstant locale,
@@ -157,6 +159,12 @@ public class WorkspaceEventsHandler {
      *         callback which is necessary to notify that workspace component started or failed
      */
     void trackWorkspaceEvents(final WorkspaceDto workspace, final Callback<Component, Exception> callback) {
+        if (workspaceEventsHandled) {
+            return;
+        }
+
+        workspaceEventsHandled = true;
+
         this.workspaceComponent = wsComponentProvider.get();
         this.messageBus = messageBusProvider.getMessageBus();
         subscribeToWorkspaceStatusEvents(workspace);
@@ -271,6 +279,10 @@ public class WorkspaceEventsHandler {
     }
 
     private void subscribeOnEnvironmentOutputChannel(WorkspaceDto workspace) {
+        if (environmentOutputSubscriptionHandler != null) {
+            return;
+        }
+
         final Link link = workspace.getLink(LINK_REL_ENVIRONMENT_OUTPUT_CHANNEL);
         final LinkParameter logsChannelLinkParameter = link.getParameter("channel");
         if (logsChannelLinkParameter == null) {
@@ -283,6 +295,10 @@ public class WorkspaceEventsHandler {
     }
 
     private void subscribeOnEnvironmentStatusChannel(WorkspaceDto workspace) {
+        if (environmentStatusSubscriptionHandler != null) {
+            return;
+        }
+
         final Link link = workspace.getLink(LINK_REL_ENVIRONMENT_STATUS_CHANNEL);
         final LinkParameter statusChannelLinkParameter = link.getParameter("channel");
         if (statusChannelLinkParameter == null) {
@@ -295,6 +311,10 @@ public class WorkspaceEventsHandler {
     }
 
     private void subscribeOnWsAgentOutputChannel(final WorkspaceDto workspace, final String wsMachineName) {
+        if (wsAgentLogSubscriptionHandler != null) {
+            return;
+        }
+
         restoreDevMachineLogs(workspace.getRuntime().getDevMachine()); //try to restore logs if workspace already started
                                                                        // and dev machine provide some output
         wsAgentLogChannel = "workspace:" + workspace.getId() + ":ext-server:output";
@@ -385,7 +405,6 @@ public class WorkspaceEventsHandler {
                     break;
 
                 case ERROR:
-                    unSubscribeHandlers();
                     notificationManager.notify(locale.workspaceStartFailed(), FAIL, FLOAT_MODE);
                     loader.setError(LoaderPresenter.Phase.STARTING_WORKSPACE_RUNTIME);
                     final String workspaceName = workspace.getConfig().getName();
@@ -400,7 +419,6 @@ public class WorkspaceEventsHandler {
 
                 case STOPPED:
                     loader.setSuccess(LoaderPresenter.Phase.STOPPING_WORKSPACE);
-                    unSubscribeHandlers();
 
                     try {
                         eventBus.fireEvent(new WorkspaceStoppedEvent(workspace));
